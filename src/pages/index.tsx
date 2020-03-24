3c40a60bd50cea380bcf8aed517603179121d3de
Renner Leite Lucena <renner@brex.com>
2020-03-23 22:06:24 -0300

    create very basic file code structure for frontend

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const IndexPage = () => (
+  <div>
+    <div>
+      Home
+    </div>
+    <Link to="/page-2/">Page 2</Link>
+  </div>
+)
+
+export default IndexPage
---
+++ src/pages/page-2.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const SecondPage = () => (
+  <div>
+    <div>
+      Page 2
+    </div>
+    <Link to="/">Home</Link>
+  </div>
+)
+
+export default SecondPage
